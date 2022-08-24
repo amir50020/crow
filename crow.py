e151f945758b4b0d81bd95ee294366aebd272957
amir500202420 <amir11228500202420@gmail,com>
2022-08-24 19:25:16 +0430

    add crow

--- crow.py
+++ crow.py
@@ -1,3 +1,24 @@
-class Crow :
-    def __init__(self):
-        pass
+class Crow:
+    def __init__(self, par_count, pa_count):
+        self.__par_count = par_count
+        self.__pa_count = pa_count
+
+    def set_par_count(self, par_count):
+        if type(par_count) != int:
+            raise Exception("this is not int")
+        if par_count < 0:
+            raise Exception("this is <0")
+        self.__par_count = par_count
+
+    def set_pa_count(self, pa_count):
+        if type(pa_count) != int:
+            raise Exception("this is not int ")
+        if pa_count < 0:
+            raise Exception("this is  <0")
+        self.__pa_count = pa_count
+
+    def get_par_count(self):
+        return self.__par_count
+
+    def get_pa_count(self):
+        return self.__pa_count
