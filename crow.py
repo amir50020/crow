f8e0675518426bfb5f40430ec460bb16101e3cb7
crow <amir112285002024@gmail.com>
2022-08-25 12:37:42 +0430

    add crow

--- crow.py
+++ crow.py
@@ -1,7 +1,27 @@
 class Crow:
-    def __init__(self, par_count, pa_count):
+
+    def __init__(self, par_count, pa_count, eyes_count):
         self.__par_count = par_count
         self.__pa_count = pa_count
+        self.__eyes_count = eyes_count
+
+    def set_eyes_count(self, eyes_count):
+        if type(eyes_count) != int:
+            raise TypeError
+        try:
+            if eyes_count < 1:
+                self.__eyes_count = eyes_count
+            else:
+                raise ValueError
+        except ValueError:
+            print("this is <1")
+        try:
+            if eyes_count > 2:
+                self.__eyes_count = eyes_count
+            else:
+                raise ValueError
+        except ValueError:
+            print("this is <2")
 
     def set_par_count(self, par_count):
         if type(par_count) != int:
@@ -22,3 +42,6 @@ class Crow:
 
     def get_pa_count(self):
         return self.__pa_count
+
+    def get_eyes_count(self):
+        return self.__eyes_count
